57105e63c10502295c45f360443537b10eaa260d
dkmityuk <71889565+dkmityuk@users.noreply.github.com>
2022-06-14 11:18:48 +0300

    Add files via upload

---
+++ ЛР1/script.js
@@ -0,0 +1,30 @@
+const cars = ['Mazda', 'Mercedes', 'Ford'];
+// console.log(cars);
+
+// for (let i = 0; i < cars.length; i++){
+//     console.log(i);
+//     const car = cars[i]
+//     // console.log(car)
+// }
+
+// greet2('Dmytro')
+
+// greet('Dima')
+
+
+
+//Function Declaration
+// const people = [
+//     {name: 'Dima', butget: 4200},
+//     {name: 'Sasha', butget: 2400},
+//     {name: 'Artem', butget: 1500},
+//     {name: 'Oleh', butget: 6000},
+// ]
+
+// const index = people.findIndex(function(person){
+//     return person.butget === 1500
+// })
+
+// console.log(person)
+
+console.log(cars.includes('Mazda'))
